b86dd7f35383f1dd85fe446f141f3fd3f792714e
Will Chen <wchen108@uw.edu>
2018-11-21 15:25:50 -0800

    Update CRISPR_NHEJ_Pipeline.sh
    Update directory

--- scripts/CRISPR_NHEJ_Pipeline.sh
+++ scripts/CRISPR_NHEJ_Pipeline.sh
@@ -20,8 +20,8 @@ python ${workdir}scripts/UMI_extractor.py -S $samplename -F $read_fwd -R $read_r
 # Merge and align two fastq files
 if [ "$merge" == "TRUE" ]; then
 	echo "Merging paired end reads."
-	#pear-0.9.6-bin-32 -f $outdir/${samplename}_fwd.fastq -r $outdir/${samplename}_rev.fastq -o $outdir/${samplename}
-	#rm $outdir/${samplename}.discarded.fastq $outdir/${samplename}.unassembled.forward.fastq $outdir/${samplename}.unassembled.reverse.fastq 
-	#needleall $ref $outdir/${samplename}.assembled.fastq -datafile /net/shendure/vol10/projects/CRISPR.OT.Homing/nobackup/ref/EDNAFULL.modified -gapopen 20 -gapextend 0.5 -aformat3 fasta -awidth3=5000 -outfile $outdir/${samplename}_needleall.fasta
+	pear-0.9.6-bin-32 -f $outdir/${samplename}_fwd.fastq -r $outdir/${samplename}_rev.fastq -o $outdir/${samplename}
+	rm $outdir/${samplename}.discarded.fastq $outdir/${samplename}.unassembled.forward.fastq $outdir/${samplename}.unassembled.reverse.fastq 
+	needleall $ref $outdir/${samplename}.assembled.fastq -datafile ${workdir}ref/EDNAFULL.modified -gapopen 20 -gapextend 0.5 -aformat3 fasta -awidth3=5000 -outfile $outdir/${samplename}_needleall.fasta
 	python ${workdir}scripts/NHEJ_analysis.py $outdir ${samplename} 254
 fi
