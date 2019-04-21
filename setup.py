0fc5697eaa06a6c8ac940bffe712c59f4c46e195
Will Chen <wchen108@uw.edu>
2019-04-20 22:29:02 -0700

    includes data

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Lindel",
-    version="0.0.1",
+    version="1.0",
     author="Wei Chen",
     author_email="wchen108@uw.edu",
     description="A package for Lindel",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/shendurelab/CRISPR_NHEJ_prediction/tree/master/scripts",
-    packages=setuptools.find_packages(),
+    packages=['Lindel'],
+    install_requires=['biopython','numpy','scipy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-)
+    include_package_data=True)
