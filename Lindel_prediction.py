916eea4fa8b4be238fe9bfad79ac933886bd6ad2
Will Chen <wchen108@uw.edu>
2019-04-22 11:12:38 -0700

    Update Lindel_prediction.py

--- Lindel_prediction.py
+++ Lindel_prediction.py
@@ -5,9 +5,10 @@
 2. Add Lindel folder to your python path.
 3. y_hat is the prediction of all ~450 classes of indels <30bp.
 4. fs is the frameshift ratio for this sequence.
-5. Input should be 65bp and the clevage site should be at 30
+5. Input should be 65bp (30 bp upstream and 35 bp downstream of the cleavage site)
+usage: pyton Lindel_predction.py your_sequence_here your_file_name_here(can be gene name or guide name you designed)
 '''
-import Lindel, os
+import Lindel, os, sys
 from Lindel.Predictor import * 
 import pickle as pkl
 
@@ -17,14 +18,13 @@ seq = sys.argv[1].upper() #input your sequence here
 filename = sys.argv[2]
 try:
     y_hat, fs = gen_prediction(seq,weights,prerequesites)
-    filename += '_frame_shift_ratio=' + str(fs)
-    print (fs)
+    filename += '_fs=' + str(round(fs,3))+'.txt'
     rev_index = prerequesites[1]
-	pred_freq = {}
-	for i in range(len(y_hat)):
-	    if y_hat[i]!=0:
-	        pred_freq[rev_index[i]] = y_hat[i]
-	pred_sorted = sorted(pred_freq.items(), key=lambda kv: kv[1],reverse=True)
-	write_file(seq,pred_sorted,pred_freq,filename)
+    pred_freq = {}
+    for i in range(len(y_hat)):
+        if y_hat[i]!=0:
+            pred_freq[rev_index[i]] = y_hat[i]
+    pred_sorted = sorted(pred_freq.items(), key=lambda kv: kv[1],reverse=True)
+    write_file(seq,pred_sorted,pred_freq,filename)
 except ValueError:
-    return ('Error: No PAM sequence is identified.')
+    print ('Error: No PAM sequence is identified.Please check your sequence and try again')
